d234d5f834e4b8238e7fef25166c7e3b2ded9db4
Massay Bah <massaybah@gmail.com>
2024-06-27 23:20:06 +0000

    update:site styles 27.06.24

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -198,7 +198,7 @@ const experience = [
     <div class="flex min-w-full min-h-screen">
         <div class="w-[100%] lg:w-[75%] bg-sky-200 p-2 space-y-4 lg:p-2 md:space-y-0">
             <div class="flex justify-between">
-                <div class="flex  justify-start p-2 mx-2  rounded-md  bg-sky-500">
+                <div class="flex  justify-start p-2 mx-2  rounded-md  bg-sky-900 text-gray-50">
                     <img class="w-20 h-auto md:w-28  md:h-28 rounded-lg" alt="Massay Bah"
                         src="/images/massaybah.jpeg" />
                     <div
@@ -209,12 +209,36 @@ const experience = [
                         <h1 class="font-semibold text-sm md:text-lg">(+220) 3199935 / 2565405</h1>
                     </div>
                 </div>
-                <div class="bg-red-900 flex">
-                    <button class="px-8 bg-indigo-300 ">Hire Me</button>
-                    <button class="px-8">LinkedIn</button>
+                
+                <div class="flex-1 border-2 rounded-md bg-green-500 grid place-content-center">
+                    <div class="flex  justify-between space-x-2">
+                        <div class="flex space-x-2">
+                            <button class="px-8 bg-indigo-300 py-2 flex items-center font-bold">
+
+                                <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z" />
+                                    </svg>
+                                </span>Hire Me</button>
+                            <button class="px-8 bg-blue-400 flex items-center font-bold">
+                                <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
+                                    </svg>
+                                </span>
+
+                                LinkedIn</button>
+                        </div>
+
+                        <div class="flex space-x-2">
+                            <button class="px-8 bg-indigo-300 py-2">Contact</button>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="p-2 flex flex-col space-y-2">
+            <div class="py-2 flex flex-col space-y-2">
                 <div class="border bg-gray-50 p-2 rounded-md">
                     <h1 class="font-semibold text-center leading-6 lg:text-2xl text-lg">Projects</h1>
                     <ol>
