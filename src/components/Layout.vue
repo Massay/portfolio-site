67cd5a2a900a7f94a45cbddc5e6b63b5a835a21a
Massay Bah <massaybah@gmail.com>
2024-06-27 23:37:08 +0000

    update:site styles 27.06.24

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -196,9 +196,9 @@ const experience = [
 
 <template>
     <div class="flex min-w-full min-h-screen">
-        <div class="w-[100%] lg:w-[75%] bg-sky-200 p-2 space-y-4 lg:p-2 md:space-y-0">
+        <div class="w-[100%] lg:w-[75%] bg-gray-300 p-2 space-y-4 lg:p-2 md:space-y-0">
             <div class="flex justify-between">
-                <div class="flex  justify-start p-2 mx-2  rounded-md  bg-sky-900 text-gray-50">
+                <div class="flex flex-1 justify-between  lg:justify-start p-2 mx-2  rounded-md  bg-sky-900 text-gray-50">
                     <img class="w-20 h-auto md:w-28  md:h-28 rounded-lg" alt="Massay Bah"
                         src="/images/massaybah.jpeg" />
                     <div
@@ -210,10 +210,10 @@ const experience = [
                     </div>
                 </div>
                 
-                <div class="flex-1 border-2 rounded-md bg-green-500 grid place-content-center">
+                <div class="lg:flex-1 border-2 rounded-md hidden bg-sky-900 lg:grid place-content-center">
                     <div class="flex  justify-between space-x-2">
                         <div class="flex space-x-2">
-                            <button class="px-8 bg-indigo-300 py-2 flex items-center font-bold">
+                            <button class="px-8 bg-gray-50 py-2 flex items-center font-bold gap-2 rounded-sm">
 
                                 <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                         stroke-width="1.5" stroke="currentColor" class="size-6">
@@ -221,19 +221,23 @@ const experience = [
                                             d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z" />
                                     </svg>
                                 </span>Hire Me</button>
-                            <button class="px-8 bg-blue-400 flex items-center font-bold">
-                                <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                        stroke-width="1.5" stroke="currentColor" class="size-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
-                                    </svg>
+                            <button class="px-8 bg-gray-50 flex items-center font-bold gap-2 rounded-sm">
+                                <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 9v.906a2.25 2.25 0 0 1-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 0 0 1.183 1.981l6.478 3.488m8.839 2.51-4.66-2.51m0 0-1.023-.55a2.25 2.25 0 0 0-2.134 0l-1.022.55m0 0-4.661 2.51m16.5 1.615a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V8.844a2.25 2.25 0 0 1 1.183-1.981l7.5-4.039a2.25 2.25 0 0 1 2.134 0l7.5 4.039a2.25 2.25 0 0 1 1.183 1.98V19.5Z" />
+</svg>
+
                                 </span>
 
-                                LinkedIn</button>
+                                Email</button>
                         </div>
 
                         <div class="flex space-x-2">
-                            <button class="px-8 bg-indigo-300 py-2">Contact</button>
+                            <button class="px-8 bg-gray-50 py-2 flex items-center gap-2 font-bold rounded-sm">
+                                <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
+</svg>
+</span>
+                                Contact</button>
                         </div>
                     </div>
                 </div>
@@ -241,11 +245,10 @@ const experience = [
             <div class="py-2 flex flex-col space-y-2">
                 <div class="border bg-gray-50 p-2 rounded-md">
                     <h1 class="font-semibold text-center leading-6 lg:text-2xl text-lg">Projects</h1>
-                    <ol>
-                        <li v-for="(project, index) in projects" :key="index" class="lg:text-lg text-sm leading-8">
-                            {{ project.name }}
-                        </li>
-                    </ol>
+                    <div v-for="(project, index) in projects" :key="index" class="lg:text-lg text-sm leading-8">
+                           <h1 class="font-mono">[{{ index + 1 }}] {{ project.name }}</h1>
+                           <p class="text-xs lg:text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione laudantium sint doloribus earum numquam impedit minima ea praesentium qui sit error quod quidem, esse id alias, exercitationem, quisquam asperiores dolor.</p>
+                    </div>
                 </div>
 
                 <div class="border  bg-gray-50 p-2 rounded-md">
@@ -295,7 +298,7 @@ const experience = [
 
         </div>
 
-        <div class="lg:w-[25%] p-2 lg:inline-block bg-sky-200 hidden">
+        <div class="lg:w-[25%] p-2 lg:inline-block bg-gray-300 hidden">
 
             <div class="bg-yellow-500 rounded-md h-[198px]">
                 Side B
