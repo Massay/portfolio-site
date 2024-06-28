3c99073be5ef4aaa6f10f5a754a834b3680ba1c9
Massay Bah <massaybah@gmail.com>
2024-06-28 09:34:31 +0000

    latest:added icons for skills

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -23,25 +23,60 @@ const projects = [
 
 const programmingLanguages = [
     {
-        name: 'C#'
+        name: 'C#',
+        logo: 'icons8-c-64.png'
     },
     {
-        name: "Php"
+        name: "Php",
+        logo: 'icons8-php-48.png'
     },
     {
-        name: "Dart"
+        name: "Laravel",
+        logo: 'icons8-laravel-48.png'
     },
     {
-        name: "Python"
+        name: "VueJs",
+        logo: 'icons8-vuejs-64.png'
     },
     {
-        name: "Html & Css"
+        name: 'Python',
+        logo: 'icons8-python-48.png'
     },
     {
-        name: "Kotlin"
+        name: 'Jenkins',
+        logo: 'icons8-jenkins-48.png'
     },
     {
-        name: "Javascript"
+        name: "Html & Css",
+        logo: 'icons8-tailwindcss-48.png'
+    },
+    {
+        name: "Java",
+        logo: 'icons8-java-64.png'
+    },
+    {
+        name: "DevOps",
+        logo: "icons8-devops-60.png"
+    },
+    {
+        name: "Git",
+        logo: "icons8-git-100.png"
+    },
+    {
+        name: "Flutter",
+        logo: "icons8-flutter-48.png"
+    },
+    {
+        name: "Kotlin",
+        logo: 'icons8-vuejs-64.png'
+    },
+    {
+        name: "Mysql",
+        logo: "icons8-mysql-48.png"
+    },
+    {
+        name: "Javascript",
+        logo: 'icons8-javascript-48.png'
     }
 ]
 
@@ -198,7 +233,8 @@ const experience = [
     <div class="flex min-w-full min-h-screen">
         <div class="w-[100%] lg:w-[75%] bg-gray-300 p-2 space-y-4 lg:p-2 md:space-y-0">
             <div class="flex justify-between">
-                <div class="flex flex-1 justify-between  lg:justify-start p-2 mx-2  rounded-md  bg-sky-900 text-gray-50">
+                <div
+                    class="flex flex-1 justify-between  lg:justify-start p-2 mx-2  rounded-md  bg-sky-900 text-gray-50">
                     <img class="w-20 h-auto md:w-28  md:h-28 rounded-lg" alt="Massay Bah"
                         src="/images/massaybah.jpeg" />
                     <div
@@ -209,7 +245,7 @@ const experience = [
                         <h1 class="font-semibold text-sm md:text-lg">(+220) 3199935 / 2565405</h1>
                     </div>
                 </div>
-                
+
                 <div class="lg:flex-1 border-2 rounded-md hidden bg-sky-900 lg:grid place-content-center">
                     <div class="flex  justify-between space-x-2">
                         <div class="flex space-x-2">
@@ -222,9 +258,11 @@ const experience = [
                                     </svg>
                                 </span>Hire Me</button>
                             <button class="px-8 bg-gray-50 flex items-center font-bold gap-2 rounded-sm">
-                                <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 9v.906a2.25 2.25 0 0 1-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 0 0 1.183 1.981l6.478 3.488m8.839 2.51-4.66-2.51m0 0-1.023-.55a2.25 2.25 0 0 0-2.134 0l-1.022.55m0 0-4.661 2.51m16.5 1.615a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V8.844a2.25 2.25 0 0 1 1.183-1.981l7.5-4.039a2.25 2.25 0 0 1 2.134 0l7.5 4.039a2.25 2.25 0 0 1 1.183 1.98V19.5Z" />
-</svg>
+                                <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M21.75 9v.906a2.25 2.25 0 0 1-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 0 0 1.183 1.981l6.478 3.488m8.839 2.51-4.66-2.51m0 0-1.023-.55a2.25 2.25 0 0 0-2.134 0l-1.022.55m0 0-4.661 2.51m16.5 1.615a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V8.844a2.25 2.25 0 0 1 1.183-1.981l7.5-4.039a2.25 2.25 0 0 1 2.134 0l7.5 4.039a2.25 2.25 0 0 1 1.183 1.98V19.5Z" />
+                                    </svg>
 
                                 </span>
 
@@ -233,10 +271,12 @@ const experience = [
 
                         <div class="flex space-x-2">
                             <button class="px-8 bg-gray-50 py-2 flex items-center gap-2 font-bold rounded-sm">
-                                <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
-</svg>
-</span>
+                                <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
+                                    </svg>
+                                </span>
                                 Contact</button>
                         </div>
                     </div>
@@ -246,27 +286,36 @@ const experience = [
                 <div class="border bg-gray-50 p-2 rounded-md">
                     <h1 class="font-semibold text-center leading-6 lg:text-2xl text-lg">Projects</h1>
                     <div v-for="(project, index) in projects" :key="index" class="lg:text-lg text-sm leading-8">
-                           <h1 class="font-mono">[{{ index + 1 }}] {{ project.name }}</h1>
-                           <p class="text-xs lg:text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione laudantium sint doloribus earum numquam impedit minima ea praesentium qui sit error quod quidem, esse id alias, exercitationem, quisquam asperiores dolor.</p>
+                        <h1 class="font-mono">[{{ index + 1 }}] {{ project.name }}</h1>
+                        <p class="text-xs lg:text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+                            laudantium sint doloribus earum numquam impedit minima ea praesentium qui sit error quod
+                            quidem, esse id alias, exercitationem, quisquam asperiores dolor.</p>
                     </div>
                 </div>
 
                 <div class="border  bg-gray-50 p-2 rounded-md">
-                    <h1 class="font-semibold text-center leading-6 lg:text-2xl text-lg">Languages</h1>
-                    <ul>
-                        <li v-for="(project, index) in programmingLanguages" :key="index">
-                            {{ project.name }}
-                        </li>
-                    </ul>
+                    <h1 class="font-semibold text-center leading-6 lg:text-2xl text-lg my-8">Skills & programming
+                        Languages</h1>
+
+                    <div class="grid grid-cols-1 lg:grid-cols-6 gap-2">
+                        <div v-for="(project, index) in programmingLanguages" :key="index"
+                            class="bg-slate-950 text-gray-50 rounded-md flex flex-col px-2 py-8 justify-center items-center">
+                            <span>
+                                <img :src="'portfolio-site/images/' + project.logo" :alt="project.name">
+                            </span>
+                            <span class="font-bold"> {{ project.name }}</span>
+                        </div>
+                    </div>
+
                 </div>
-                <div class="  bg-gray-50 p-2 rounded-md">
+                <!-- <div class="  bg-gray-50 p-2 rounded-md">
                     <h1 class="font-semibold text-center leading-6 lg:text-2xl text-lg">Frameworks</h1>
                     <ul>
                         <li v-for="(project, index) in frameworks" :key="index">
                             {{ project.name }}
                         </li>
                     </ul>
-                </div>
+                </div> -->
                 <div class=" bg-gray-50 p-2 rounded-md">
                     <h1 class="text-center leading-6 lg:text-2xl text-lg my-2 font-semibold">Experience</h1>
 
